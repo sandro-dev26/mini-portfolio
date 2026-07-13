339814ca712ec707d564899a26248eddad43ef2d
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-13 21:53:39 +0400

    Update skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Hero from "./sections/Hero";
 import Timeline from "./sections/timeline";
+import Skills from "./sections/skills";
 import SunIcon from "./components/sunicon";
 import MoonIcon from "./components/moonicon";
 
 function App() {
   const [darkModeTheme, setDarkModeTheme] = useState<boolean>(true);
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+
+    if (!body) return;
+
+    if (darkModeTheme) {
+      body.classList.remove("light");
+    } else {
+      body.classList.add("light");
+    }
+  }, [darkModeTheme]);
 
   function changeTheme() {
     setDarkModeTheme(!darkModeTheme);
@@ -14,7 +27,7 @@ function App() {
 
   return (
     <div
-      className={`${darkModeTheme ? "bg-slate-950 text-neutral-50" : "bg-slate-50 text-neutral-900"} w-full h-screen transition-all duration-200`}
+      className={`${darkModeTheme ? "bg-slate-950 text-neutral-50" : "bg-slate-50 text-neutral-900"} w-full h-full transition-all duration-200`}
     >
       <button
         className={`${darkModeTheme ? "bg-slate-800 hover:bg-slate-700 active:bg-slate-950" : "bg-slate-100 hover:bg-slate-200 active:bg-slate-50"} m-4 rounded-full p-2 transition-all duration-300 active:scale-[0.9]`}
@@ -25,6 +38,7 @@ function App() {
       <Hero darkMode={darkModeTheme} />
       <main>
         <Timeline darkMode={darkModeTheme} />
+        <Skills darkMode={darkModeTheme} />
       </main>
     </div>
   );
--- src/sections/skills.tsx
+++ src/sections/skills.tsx
@@ -2,6 +2,81 @@ type TimelineProps = {
   darkMode: boolean;
 };
 
-function Skills({ darkMode }: TimelineProps) {}
+function Skills({ darkMode }: TimelineProps) {
+  return (
+    <>
+      <div
+        className={`flex flex-col items-start m-4 mt-12 mb-4 p-2 rounded-xl w-[90%] md:w-[50%] lg:w-[70%] transition-all duration-200 ${darkMode ? "bg-slate-800 hover:bg-slate-700" : "bg-slate-200 hover:bg-slate-300"}`}
+      >
+        <span className="m-2 mb-4 text-2xl font-light">Skills</span>
+        <ul className="grid grid-cols-4 gap-4 m-2 md:grid-cols-6 md:gap-6 lg:grid-cols-8 lg:gap-8">
+          <li>
+            <img
+              className="w-12 h-12 rounded-sm md:w-16 md:h-16 md:rounded-md lg:w-20 lg:h-20 lg:rounded-lg transition-all duration-200 hover:scale-[1.1] hover:brightness-90"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+              alt="Javascript"
+            />
+          </li>
+          <li>
+            <img
+              className="w-12 h-12 rounded-sm md:w-16 md:h-16 md:rounded-md lg:w-20 lg:h-20 lg:rounded-lg transition-all duration-200 hover:scale-[1.1] hover:brightness-90"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+              alt="Typescript"
+            />
+          </li>
+          <li>
+            <img
+              className="w-12 h-12 rounded-sm md:w-16 md:h-16 md:rounded-md lg:w-20 lg:h-20 lg:rounded-lg transition-all duration-200 hover:scale-[1.1] hover:brightness-90"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+              alt="React"
+            />
+          </li>
+          <li>
+            <img
+              className="w-12 h-12 rounded-sm md:w-16 md:h-16 md:rounded-md lg:w-20 lg:h-20 lg:rounded-lg transition-all duration-200 hover:scale-[1.1] hover:brightness-90"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg"
+              alt="Tailwind Css"
+            />
+          </li>
+          <li>
+            <img
+              className="w-12 h-12 rounded-sm md:w-16 md:h-16 md:rounded-md lg:w-20 lg:h-20 lg:rounded-lg transition-all duration-200 hover:scale-[1.1] hover:brightness-90"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+              alt="Git"
+            />
+          </li>
+          <li>
+            <img
+              className="w-12 h-12 rounded-sm md:w-16 md:h-16 md:rounded-md lg:w-20 lg:h-20 lg:rounded-lg transition-all duration-200 hover:scale-[1.1] hover:brightness-90"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+              alt="GitHub"
+            />
+          </li>
+          <li>
+            <img
+              className="w-12 h-12 rounded-sm md:w-16 md:h-16 md:rounded-md lg:w-20 lg:h-20 lg:rounded-lg transition-all duration-200 hover:scale-[1.1] hover:brightness-90"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vitejs/vitejs-original.svg"
+              alt="Vite"
+            />
+          </li>
+          <li>
+            <img
+              className="w-12 h-12 rounded-sm md:w-16 md:h-16 md:rounded-md lg:w-20 lg:h-20 lg:rounded-lg transition-all duration-200 hover:scale-[1.1] hover:brightness-90"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+              alt="Html"
+            />
+          </li>
+          <li>
+            <img
+              className="w-12 h-12 rounded-sm md:w-16 md:h-16 md:rounded-md lg:w-20 lg:h-20 lg:rounded-lg transition-all duration-200 hover:scale-[1.1] hover:brightness-90"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+              alt="Css"
+            />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
 
 export default Skills;
