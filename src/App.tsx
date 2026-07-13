305ceb5e99d38e4dbad567b48e93986644a807e3
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-13 23:56:04 +0400

    styled scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,17 @@ function App() {
 
   useEffect(() => {
     const body = document.querySelector("body");
+    const html = document.querySelector("html");
 
     if (!body) return;
+    if (!html) return;
 
     if (darkModeTheme) {
       body.classList.remove("light");
+      html.classList.remove("light");
     } else {
       body.classList.add("light");
+      html.classList.add("light");
     }
   }, [darkModeTheme]);
 
@@ -29,7 +33,7 @@ function App() {
 
   return (
     <div
-      className={`${darkModeTheme ? "bg-slate-950 text-neutral-50" : "bg-slate-50 text-neutral-900"} w-full h-full transition-all duration-200`}
+      className={`${darkModeTheme ? "bg-slate-950 text-neutral-50" : "bg-slate-50 text-neutral-900"} w-full min-h-screen transition-all duration-200`}
     >
       <button
         className={`${darkModeTheme ? "bg-slate-800 hover:bg-slate-700 active:bg-slate-950" : "bg-slate-100 hover:bg-slate-200 active:bg-slate-50"} m-4 rounded-full p-2 transition-all duration-300 active:scale-[0.9]`}
