367a7f8bb09f62d237fdb15bc7f70fe7523c0b92
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-13 18:44:30 +0400

    Update hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,17 +6,15 @@ type HeroProps = {
 
 function Hero({ darkMode }: HeroProps) {
   return (
-    <>
-      <header className="m-4 w-[80%]">
-        <h1 className={`text-3xl font-semibold mb-2`}>My Mini Portfolio</h1>
-        <span
-          className={`text-sm font-light hover:text-neutral-300 ${darkMode ? "hover:text-neutral-300" : "hover:text-neutral-600"}`}
-        >
-          A small portfolio showcasing my journey and progress through web
-          development.
-        </span>
-      </header>
-    </>
+    <header className="m-4 w-[80%]">
+      <h1 className={`text-3xl font-semibold mb-2`}>My Mini Portfolio</h1>
+      <span
+        className={`text-sm font-light hover:text-neutral-300 ${darkMode ? "hover:text-neutral-300" : "hover:text-neutral-600"}`}
+      >
+        A small portfolio showcasing my journey and progress through web
+        development.
+      </span>
+    </header>
   );
 }
 
