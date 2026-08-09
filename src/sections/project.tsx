071953d859d11254a9cfef3b4b60a76ae4a60373
sandro-dev26 <sadrokakhidze@gmail.com>
2026-08-09 20:53:09 +0400

    Update Projects page and projects data handling

--- src/sections/project.tsx
+++ src/sections/project.tsx
@@ -1,6 +1,4 @@
-import weatherApp from "../assets/weatherApp.png";
-import formulaSarcastic from "../assets/formulaSarcastic.png";
-import miniF1Dashboard from "../assets/miniF1Dashboard.png";
+import projectsData from "../data/projects.json";
 
 type TimelineProps = {
   darkMode: boolean;
@@ -14,84 +12,50 @@ function Projects({ darkMode }: TimelineProps) {
       >
         <span className="m-2 mb-4 text-2xl font-light">Projects</span>
         <ul className="flex flex-col gap-4 m-2">
-          <li className="flex flex-col w-[100%] overflow-hidden">
-            <span className="text-lg font-semibold">Formula Sarcastic</span>
-            <img
-              className="w-full h-full object-contain rounded-xl transition-all duration-200 hover:brightness-80 mt-4 mb-2"
-              src={formulaSarcastic}
-              alt="Weather App Png"
-            />
-            <span>Not Hosted</span>
-            <span>No GitHub Repo</span>
-            <span>
-              Description:{" "}
-              <span className="text-sm font-light">
-                A fun JavaScript project built around Formula 1 memes and images
-                to practice interactive web development.
-              </span>
-            </span>
-          </li>
-          <li className="flex flex-col w-[100%] overflow-hidden">
-            <span className="text-lg font-semibold">Weather App</span>
-            <img
-              className="w-full h-full object-contain rounded-xl transition-all duration-200 hover:brightness-80 mt-4 mb-2"
-              src={weatherApp}
-              alt="Weather App Png"
-            />
-            <span>Not Hosted</span>
-            <span>No GitHub Repo</span>
-            <span>
-              Description:{" "}
-              <span className="text-sm font-light">
-                A personal project built to improve my TypeScript skills by
-                working with APIs.
-              </span>
-            </span>
-          </li>
-          <li className="flex flex-col gap-1 w-[100%] overflow-hidden">
-            <span className="text-lg font-semibold">Mini F1 Dashboard</span>
-            <img
-              className="w-full h-full object-contain rounded-xl transition-all duration-200 hover:brightness-80 mt-4 mb-2"
-              src={miniF1Dashboard}
-              alt="Weather App Png"
-            />
-            <span>
-              URL:{" "}
-              <a
-                href="https://mini-f1-dashboard.vercel.app/"
-                className="font-light translate-all duration-100 hover:font-normal"
-              >
-                Mini F1 Dashboard
-              </a>
-            </span>
-            <span className="flex flex-row items-center gap-1">
-              Repo:
-              <a
-                href="https://github.com/sandro-dev26/mini-f1-dashboard"
-                className="flex flex-row items-center gap-1 font-light"
-              >
-                <img
-                  src={
-                    darkMode
-                      ? "https://cdn.simpleicons.org/github/ffffff"
-                      : "https://cdn.simpleicons.org/github/000000"
-                  }
-                  className="w-4 h-4"
+          {projectsData &&
+            projectsData.map((project) => (
+              <li className="flex flex-col gap-1 w-full overflow-hidden">
+                <span className="text-lg font-semibold">{project.name}</span>
+                <iframe
+                  className="w-full h-96 object-contain rounded-xl mt-4 mb-2"
+                  src={project.url}
                 />
-                <span className="translate-all duration-100 hover:font-normal">
-                  Mini F1 Dashboard
+                <span>
+                  URL:{" "}
+                  <a
+                    href={project.url}
+                    className="font-light translate-all duration-100 hover:font-normal"
+                  >
+                    {project.name}
+                  </a>
                 </span>
-              </a>
-            </span>
-            <span>
-              Description:{" "}
-              <span className="text-sm font-light">
-                An unofficial fan-made Formula 1 dashboard built with React,
-                TypeScript, and Tailwind CSS to display race and championship
-                data.
-              </span>
-            </span>
-          </li>
+                <span className="flex flex-row items-center gap-1">
+                  Repo:
+                  <a
+                    href={project.github}
+                    className="flex flex-row items-center gap-1 font-light"
+                  >
+                    <img
+                      src={
+                        darkMode
+                          ? "https://cdn.simpleicons.org/github/ffffff"
+                          : "https://cdn.simpleicons.org/github/000000"
+                      }
+                      className="w-4 h-4"
+                    />
+                    <span className="translate-all duration-100 hover:font-normal">
+                      {project.name}
+                    </span>
+                  </a>
+                </span>
+                <span>
+                  Description:{" "}
+                  <span className="text-sm font-light">
+                    {project.description}
+                  </span>
+                </span>
+              </li>
+            ))}
         </ul>
       </div>
     </>
